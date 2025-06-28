28f2f5883874ae0882b4b05ea04ccf4c95685932
fivecat.fun <dev.freechat.fun@gmail.com>
2025-06-28 11:55:14 +0800

    refactor: adjust scripts

--- scripts/build-cpu.sh
+++ scripts/build-cpu.sh
@@ -5,7 +5,7 @@ source $(dirname ${BASH_SOURCE[0]})/setenv.sh
 check_docker
 
 mkdir -p "${DOCKER_CONFIG_HOME}/data/nls"
-cp "${PROJECT_PATH}/src/"* "${DOCKER_CONFIG_HOME}/data/"
+cp "${PROJECT_PATH}/src/"*.* "${DOCKER_CONFIG_HOME}/data/"
 cp -r "${PROJECT_PATH}/src/nls/"* "${DOCKER_CONFIG_HOME}/data/nls/"
 
 # Display configuration if VERBOSE is set
--- scripts/test-tts.sh
+++ scripts/test-tts.sh
@@ -4,13 +4,14 @@ set -e
 
 source $(dirname ${BASH_SOURCE[0]})/setenv.sh
 
+serv_addr=${ARGS[0]:-http://127.0.0.1:9880}
 zh_text="蝼蚁！赶紧叫我大王，否则打得你们呱呱叫！"
 en_text="Ants! Call me the king now, or I will beat you to a pulp!"
-ref_audio=${PROJECT_PATH}/t_voice.aac
+ref_audio=${PROJECT_PATH}/t_voice.mp3
 ref_lang=en
 ref_text="You think you were born? Hum, no, you were built."
 
-ref_path=`curl -s 'http://127.0.0.1:9880/upload_refer_audio' \
+ref_path=`curl -s "${serv_addr}/upload_refer_audio" \
   -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36' \
   -H 'Accept: text/plain, */*' \
   -F audio_file=@${ref_audio}
@@ -35,7 +36,7 @@ EOF
 
 output_audio=${PROJECT_PATH}/local-data/output/zh_sample.aac
 
-curl -X POST 'http://127.0.0.1:9880/tts' \
+curl -X POST "${serv_addr}/tts" \
   -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36' \
   -H 'Content-Type: application/json' \
   -H 'Accept: audio/*' \
@@ -57,7 +58,7 @@ EOF
 
 output_audio=${PROJECT_PATH}/local-data/output/en_sample.aac
 
-curl -X POST 'http://127.0.0.1:9880/tts' \
+curl -X POST "${serv_addr}/tts" \
   -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36' \
   -H 'Content-Type: application/json' \
   -H 'Accept: audio/*' \
